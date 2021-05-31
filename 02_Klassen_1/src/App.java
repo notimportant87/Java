50a2522813fb3bc0e279b93a2291d323dfe19b91
notimportant <torben.q@web.de>
2021-05-31 14:27:09 +0200

    add App z1-14 +

---
+++ 02_Klassen_1/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    
+    public static void main(String[] args) {
+           
+        Cat cat = new Cat();
+        System.out.println("Blick von aussen: " + cat);
+        
+        cat.tellYourAddress();
+
+
+
+
+    }
+}
